4be51a25fed88b099d623749683e7deaf136fc96
ProtoByter <bobrow2006@gmail.com>
2021-12-12 08:41:04 +0000

    Create hiro.cmake

---
+++ hiro.cmake
@@ -0,0 +1,18 @@
+include(FetchContent)
+
+function(getHiro)
+    FetchContent_Declare(hiro
+            GIT_REPOSITORY  https://github.com/ProtoByter/hiro
+            GIT_SHALLOW     TRUE
+            SOURCE_DIR      ${CMAKE_BINARY_DIR}/hiro-src
+            )
+
+    FetchContent_GetProperties(hiro)
+    if(NOT hiro_POPULATED)
+        message(STATUS "Fetching hiro")
+        FetchContent_Populate(hiro)
+        message(STATUS "Fetched hiro")
+    endif()
+
+    add_subdirectory(${hiro_SOURCE_DIR})
+endfunction()
